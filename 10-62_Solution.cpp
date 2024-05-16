b299cf545aba73e5d13f661959f3578c7173d93b
Leechihee <119794981+Leechihee@users.noreply.github.com>
2024-05-16 22:25:01 +0900

    Create 10-62_Solution.cpp

---
+++ 10-62_Solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Custom_tree.h"
+
+void output(int num)
+{
+	std::cout << num << " ";
+}
+
+int main()
+{
+	using namespace std;
+	int count, temp;
+	cout << "입력할 데이터의 개수를 입력하시오. : ";
+	(cin>>count).ignore(2,'\n');
+	cout << "데이터를 입력하시오 : ";
+	BStree<int> data;
+	for(int i = 0;i<count;i++)
+	{
+		cin >> temp;
+		data.Insert(temp);
+	}
+	
+	cout << "Preorder Traversal Sequence is : "; data.Preorder(output);
+	cout << "\nInorder Traversal Sequence is : "; data.Inorder(output);
+	cout << "\nPostorder Traversal Sequence is : "; data.Postorder(output);
+	cout << endl;
+	
+	return 0;
+}
